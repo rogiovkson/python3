d5c43df1c109a78b37254c14b5dd04bfbee4a336
rogiovkson <78314566+rogiovkson@users.noreply.github.com>
2021-02-15 19:56:53 +0300

    Update task6.py

--- hw5/task6/task6.py
+++ hw5/task6/task6.py
@@ -6,3 +6,9 @@ with open('task6.txt', 'r', encoding='utf-8') as my_file:
         num_list = [int(num) for num in filter(lambda num: num.isnumeric(), line)]
 
         print(sum(num_list))
+        
+#хотел добавить что-то типо:     
+# for key,value in dictionary:
+#         key = line[0]
+#         value = sum(num_list)
+# но не понимаю как это правильно оформить
